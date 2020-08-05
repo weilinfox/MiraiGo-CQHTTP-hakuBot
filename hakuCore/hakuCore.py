f44e8a0ee4545f00cbb610dd37e1f744ccb4c821
weilinfox <weilin.king@qq.com>
2020-08-05 22:51:02 +0800

    haku reply

--- hakuCore/hakuCore.py
+++ hakuCore/hakuCore.py
@@ -6,6 +6,7 @@ from importlib import import_module
 from hakuCore.botApi import *
 
 def haku (msgList):
+    # 分发命令
     if (msgList['raw_message'][0] == ':'):
         req = list(msgList['raw_message'].split(' ', 1))
         try:
@@ -13,6 +14,21 @@ def haku (msgList):
         except:
             pass
         else:
-            print()
             plgs.main(msgList)
             return
+
+    # 命令以外的处理
+    if msgList['raw_message'].strip() == '小白':
+        if msgList['message_type'] == 'private':
+            send_private_message(msgList['user_id'], '小白在呢~')
+        elif msgList['message_type'] == 'group':
+            send_group_message(msgList['group_id'], '小白在呢~')
+    else:
+        for pos in range(0, len(msgList['raw_message'])-1):
+            #print(msgList['raw_message'][pos:pos+2])
+            if msgList['raw_message'][pos:pos+2] == '小白':
+                if msgList['message_type'] == 'private':
+                    send_private_message(msgList['user_id'], '[CQ:face,id=175]')
+                elif msgList['message_type'] == 'group':
+                    send_group_message(msgList['group_id'], '[CQ:face,id=175]')
+                break
